c86a22d05add3c0f1c63fcabd135651b8e395775
xzatkot <99797799+xzatkot@users.noreply.github.com>
2023-05-31 17:29:09 +0200

    Add files via upload
    Source files added

---
+++ nano_mic_src.ino
@@ -0,0 +1,60 @@
+#include <I2S.h>
+
+#define SENSITIVITY -26 // -26dBV/Pa ... 50mV/Pa
+#define SPLref 94
+#define SAMPLING_FREQ 8000
+#define FAST 8
+
+float samples[SAMPLING_FREQ/FAST];
+int j = 0;
+float SPL = 0;
+int second = 0;
+float values[FAST];
+float final = 0;
+
+void setup() {
+  // Serial.begin(9600);
+
+  // while (!Serial) { ; }
+  if (!I2S.begin(I2S_PHILIPS_MODE, SAMPLING_FREQ, 32)) {
+  //   Serial.println("Failed to initialize I2S!");
+    while (1) ;
+  }
+}
+
+void loop() {
+  if (I2S.available()) {
+    int sample = I2S.read();
+    float amplitude = sample / 2147483647.0f; // Convert sample to a float value from the interval <-1.0;1.0>
+    samples[j] = abs(amplitude);
+    j++;
+
+    if (j == SAMPLING_FREQ/FAST) {
+      float sum = 0;
+      for (int i = 0; i < SAMPLING_FREQ/FAST; i++) {
+        sum += samples[i];
+      }
+      float average = sum / (SAMPLING_FREQ/FAST);
+      float voltage = average * 3.3;
+      float dBV = 20 * log10(voltage / 1);
+      SPL = (dBV - SENSITIVITY) + SPLref;
+      values[second] = SPL;
+      second++;
+      if (second == FAST) {
+        float secondSum = 0;
+        for (int k = 0; k < FAST; k++) {
+          secondSum += values[k];
+        }
+        final = secondSum/FAST;
+        // Serial.println(final);
+        float temp = final*10;
+        int tempInt = temp;
+        int sentValue = map(tempInt, 400, 1200, 0, 255);
+        analogWrite(A0, sentValue);
+        // Serial.println(tempInt);
+        second = 0;
+      }
+      j = 0;
+    }
+  }
+}
---
+++ nodemcu_adc.ino
@@ -0,0 +1,153 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_BME280.h>
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+Adafruit_BME280 bme;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+const char* ssid = "zetak1_src";
+const char* password = "deci58bel";
+const char* host = "www.zetak1.com";
+const char* path = "/data.php";
+
+int second = 0;
+int minute = 0;
+int hour = 0;
+
+float SPL = 0;
+float temperature = 0;
+float pressure = 0;
+float humidity = 0;
+
+WiFiClientSecure client;
+DynamicJsonDocument doc(8192);
+JsonArray data = doc.to<JsonArray>();
+
+void setup() {
+  // Serial.begin(115200);
+  Wire.begin();
+
+  if (!bme.begin(0x76)) {
+    // Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    // Serial.println("Connecting to WiFi...");
+  }
+  // Serial.println("Connected to WiFi");
+
+  client.setInsecure();
+
+  const int httpPort = 443;
+  if (!client.connect(host, httpPort)) {
+    // Serial.println("connection failed");
+    return;
+  }
+}
+
+void loop() {
+  int analogValue = analogRead(A0);
+  float voltage = analogValue * (3.3 / 1023.0);
+  float receivedDataTemp = mapFloat(voltage, 0.0, 3.3, 40.0, 120.0);
+  float temperatureTemp = bme.readTemperature();
+  float pressureTemp = bme.readPressure() / 100.0F;
+  float humidityTemp = bme.readHumidity();
+
+  // Serial.println(receivedDataTemp);
+  // Serial.println(temperatureTemp);
+  // Serial.println(pressureTemp);
+  // Serial.println(humidityTemp);
+
+  SPL += receivedDataTemp;
+  temperature += temperatureTemp;
+  pressure += pressureTemp;
+  humidity += humidityTemp;
+  second++;
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("LA: ");
+  display.print(receivedDataTemp, 1);
+  display.println(" dBA");
+  display.print("Temp: ");
+  display.print(temperatureTemp, 1);
+  display.drawCircle(63, 9, 1, SSD1306_WHITE);
+  display.println(" C");
+  display.print("Baro: ");
+  display.print(pressureTemp, 1);
+  display.println(" hPa");
+  display.print("Humid: ");
+  display.print(humidityTemp, 1);
+  display.println(" %");
+  display.display();
+
+  if (second == 60) {
+    JsonObject reading = data.createNestedObject();
+    reading["LA"] = round(SPL/60*10)/10;
+    reading["Temperature"] = round(temperature/60*10)/10;
+    reading["Pressure"] = round(pressure/60*10)/10;
+    reading["Humidity"] = round(humidity/60*10)/10;
+    second = 0;
+    minute++;
+    SPL = 0;
+    temperature = 0;
+    pressure = 0;
+    humidity = 0;
+  }
+
+  if (minute == 5) {
+    sendJSONtoServer();
+    doc.clear();
+    data = doc.to<JsonArray>();
+    minute = 0;
+  }
+
+  delay(1000);
+}
+
+void sendJSONtoServer() {
+  if ((WiFi.status() == WL_CONNECTED)) { // Check the current connection status
+    HTTPClient http;
+    
+    String url = String("https://") + host + "/data.php";
+    http.begin(client, url);
+    http.addHeader("Content-Type", "application/json"); // Specify content-type header
+
+    String jsonString;
+    serializeJson(doc, jsonString); //Convert JSON document to string
+    // Serial.println(jsonString);
+    int httpResponseCode = http.POST(jsonString); // Send the POST request
+
+    if (httpResponseCode>0) {
+      String response = http.getString(); // Get the response
+      // Serial.println(httpResponseCode);
+      // Serial.println(response);
+    }
+    else {
+      // Serial.print("Error on sending POST: ");
+      // Serial.println(http.errorToString(httpResponseCode).c_str());
+    }
+    http.end();
+  }
+}
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
